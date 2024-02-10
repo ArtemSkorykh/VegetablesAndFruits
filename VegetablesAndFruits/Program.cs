1122e87914c53170137f4905bb29d8e69b10b17c
Artem <Skor_v5xz@student.itstep.org>
2024-02-10 13:03:17 +0100

    Final

--- VegetablesAndFruits/Program.cs
+++ VegetablesAndFruits/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32.SafeHandles;
 using System.Data.SqlTypes;
 using System.Data.SqlClient;
+using System;
 
 namespace VegetablesAndFruits
 {
@@ -10,7 +11,7 @@ namespace VegetablesAndFruits
         public static string StrConn => @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=VegetablesAndFruits;Integrated Security=True;Connect Timeout=30;";
         private SqlConnection _conn;
 
-
+        //// #3
         void DisplayOfAllInformation()
         {
             Console.Clear();
@@ -153,7 +154,243 @@ namespace VegetablesAndFruits
             }
         }
 
+        //// #4
+        void DisplayNumberOfVegetables()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT COUNT(VegetablesAndFruits.Id) FROM VegetablesAndFruits WHERE VegetablesAndFruits.Type = 'vegetable'";
+                var cmd = new SqlCommand(query, conn);
 
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string str = reader[0].ToString();
+                        Console.WriteLine(str);
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayNumberOfFruits()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT COUNT(VegetablesAndFruits.Id) FROM VegetablesAndFruits WHERE VegetablesAndFruits.Type = 'fruit'";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string str = reader[0].ToString();
+                        Console.WriteLine(str);
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayNumberOfGivenColor()
+        {
+            Console.Clear();
+
+            string str;
+            Console.Write("Enter the color: ");
+            str = Console.ReadLine();
+
+            Console.WriteLine();
+
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = $"SELECT * FROM VegetablesAndFruits WHERE VegetablesAndFruits.color = @cl";
+                var cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@cl", str);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Color"]} {reader["Kcal"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+        void DisplayNumbersOfEveryColor()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT VegetablesAndFruits.Color, VegetablesAndFruits.Type, COUNT(*) AS 'Number' FROM VegetablesAndFruits GROUP BY VegetablesAndFruits.Color, VegetablesAndFruits.Type";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Color"]} {reader["Type"]} {reader["Number"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayWithCaloriesBelow()
+        {
+            Console.Clear();
+
+            float f;
+            Console.Write("Enter the kcal: ");
+            f = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = $"SELECT * FROM VegetablesAndFruits WHERE VegetablesAndFruits.Kcal < @f";
+                var cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@f", f);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Color"]} {reader["Kcal"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+        void DisplayWithCaloriesHigher()
+        {
+            Console.Clear();
+
+            float f;
+            Console.Write("Enter the kcal: ");
+            f = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = $"SELECT * FROM VegetablesAndFruits WHERE VegetablesAndFruits.Kcal > @f";
+                var cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@f", f);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Color"]} {reader["Kcal"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+        void DisplayWithcalorieContentInTheIndicatedRange()
+        {
+            Console.Clear();
+
+            float f1;
+            Console.Write("Enter the first kcal: ");
+            f1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            float f2;
+            Console.Write("Enter the second kcal: ");
+            f2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = $"SELECT * FROM VegetablesAndFruits WHERE VegetablesAndFruits.Kcal > @f1 AND VegetablesAndFruits.Kcal < @f2";
+                var cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@f1", f1);
+                cmd.Parameters.AddWithValue("@f2", f2);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Color"]} {reader["Kcal"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayAllYellowOrRedColor()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT * FROM VegetablesAndFruits WHERE VegetablesAndFruits.Color = 'red' OR VegetablesAndFruits.Color = 'yellow'";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Color"]} {reader["Kcal"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
 
 
 
@@ -192,8 +429,13 @@ namespace VegetablesAndFruits
                     int j = -1;
                     while (j != 0)
                     {
-                        Console.WriteLine("Enter the comand \n1 - Display Of All Information  | 2 - Display Of All Names        | 3 - Display All Colors");
-                        Console.WriteLine("4 - Display The Maximum Calorie | 5 - Display The Minimum Calorie | 6 - Display The Average Calorie\n0 - Exit\n");
+                        Console.WriteLine("Enter the comand \n1 - Display Of All Information              | 2 - Display Of All Names         | 3 - Display All Colors");
+                        Console.WriteLine("4 - Display The Maximum Calorie             | 5 - Display The Minimum Calorie  | 6 - Display The Average Calorie");
+                        Console.WriteLine("7 -  Display Number Of Vegetables           | 8 - Display Number Of Fruits     | 9 - Display Number Of Plants Of Given Color");
+                        Console.WriteLine("10 - Display Numbers Of Every Color         | 11 - Display With Calories Below | 12 - Display With Calories Higher");
+                        Console.WriteLine("13 - Display All Plants Yellow Or Red Color | 14 - Display With Calorie Content In The Indicated Range");
+                        Console.WriteLine("0 - Exit");
+
                         Console.Write("Comand: ");
                         j = Int32.Parse(Console.ReadLine());
                         switch(j)
@@ -215,6 +457,31 @@ namespace VegetablesAndFruits
                             case 6:
                                 Program.DisplayTheAverageCalorie();
                                 break;
+                            case 7:
+                                Program.DisplayNumberOfVegetables();
+                                break;
+                            case 8:
+                                Program.DisplayNumberOfFruits();
+                                break;
+                            case 9:
+                                Program.DisplayNumberOfGivenColor();
+                                break;
+                            case 10:
+                                Program.DisplayNumbersOfEveryColor();
+                                break;
+                            case 11:
+                                Program.DisplayWithCaloriesBelow();
+                                break;
+                            case 12:
+                                Program.DisplayWithCaloriesHigher();
+                                break;
+                            case 13:
+                                Program.DisplayAllYellowOrRedColor();
+                                break;
+                            case 14:
+                                Program.DisplayWithcalorieContentInTheIndicatedRange();
+                                break;
+
                             default:
                                 j = 0;
                                 break;
