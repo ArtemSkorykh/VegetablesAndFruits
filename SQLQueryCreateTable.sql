588b0a82b829a97387064b5ac69304cbf74be4ee
Artem_Skorykh <102030527+ArtemSkorykh@users.noreply.github.com>
2024-02-12 08:25:25 +0100

    Add files via upload

---
+++ SQLQueryCreateTable.sql
@@ -0,0 +1,10 @@
+﻿USE VegetablesAndFruits;
+
+CREATE TABLE VegetablesAndFruits
+(
+	Id      INT PRIMARY KEY IDENTITY(1,1),
+	[Name]  NVARCHAR(100) NOT NULL,
+	[Type]  NVARCHAR(100) NOT NULL,
+	Color   NVARCHAR(100) NOT NULL,
+	Kcal    FLOAT NOT NULL
+)
---
+++ SQLQueryTable.sql
@@ -0,0 +1,8 @@
+﻿USE VegetablesAndFruits;
+
+INSERT INTO VegetablesAndFruits(Name, Type, color, Kcal)
+VALUES ('Apple', 'fruit', 'red', 47.5),
+       ('Orange', 'fruit', 'orange', 70),
+       ('Tomate', 'vegetable', 'red', 30),
+       ('Cucumber', 'vegetable', 'green', 29),
+       ('Pumpkin', 'vegetable', 'orange', 150)
